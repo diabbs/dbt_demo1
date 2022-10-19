baf6e7506211332069524aed6c464cd365d24ba1
mdiaby224@gmail.com <mdiaby224@gmail.com>
2022-10-19 22:22:17 +0000

    folder learn_dbt materialized to table

---
+++ dbt_project_demo/models/learn_dbt/query_result.sql
@@ -0,0 +1,5 @@
+select *
+from {{ ref('bikeshare_stations') }} bikeshare_stations
+inner join {{ ref( 'bikeshare_trips')}} bikeshare_trip
+on bikeshare_stations.station_id = bikeshare_trip.start_station_id
+where 1 = 1
